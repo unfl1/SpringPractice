1835399334d5d9575b26670bc728cf4a3e6e48c6
unfl1 <flxmaut10@gmail.com>
2025-08-01 22:30:38 +0900

    프로젝트 시작

---
+++ src/test/java/springpt/spt/SptApplicationTests.java
@@ -0,0 +1,13 @@
+package springpt.spt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
